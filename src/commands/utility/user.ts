ae64c627e3c13a0305867e67003caad588f2c275
Matt <guitarzero123@gmail.com>
2025-10-22 06:51:29 -0400

    creating commands folder and following discord js guide to learn about 'slash commands'

---
+++ src/commands/utility/user.ts
@@ -0,0 +1,11 @@
+import { SlashCommandBuilder } from 'discord.js';
+import type { CommandInteraction } from 'discord.js';
+
+module.exports = {
+	data: new SlashCommandBuilder().setName('user').setDescription('Provides information about the user'),
+	async execute(interaction: CommandInteraction) {
+		await interaction.reply(`This command was run by ${interaction.user.username}.`);
+		//interaction.member may be null and member has type(s) GuildMember | APIInteractionGuildMember whose joinedAt/joined_at properties are named inconsistently
+		//await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+	}
+}
